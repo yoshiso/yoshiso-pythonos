8f91d475ce49da7955250ba752f0519d4047596e
yss44 <nya060@gmail.com>
2014-05-27 00:04:35 +0900

    initial

---
+++ attributes/default.rb
@@ -0,0 +1,11 @@
+#
+# Cookbook Name:: pythonos
+# Recipe:: default
+#
+# Copyright 2014, Sho Yoshida
+#
+# All rights reserved - Do Not Redistribute
+#
+
+default['pyenv']['versions'] = ['2.7.6']
+default['pyenv']['version']  = "2.7.6"
---
+++ recipes/default.rb
@@ -0,0 +1,11 @@
+#
+# Cookbook Name:: pythonos
+# Recipe:: default
+#
+# Copyright 2014, Sho Yoshida
+#
+# All rights reserved - Do Not Redistribute
+#
+
+include_recipe 'yoshiso-base'
+include_recipe 'yoshiso-pyenv'
