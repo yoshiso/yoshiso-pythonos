919ec09457c34035cf7a20ba7739e1fdda70e77d
yss44 <nya060@gmail.com>
2014-06-11 23:37:41 +0900

    update prompt

--- attributes/default.rb
+++ attributes/default.rb
@@ -7,6 +7,6 @@
 # All rights reserved - Do Not Redistribute
 #
 
-default['prompt'] = "(*ﾟ▽ﾟ*)py"
+default['prompt'] = "\[\033[36m\](*ﾟ▽ﾟ*)py\[\033[00m\]"
 default['pyenv']['versions'] = ['2.7.6']
 default['pyenv']['version']  = "2.7.6"
